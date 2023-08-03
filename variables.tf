b8c561cca3647977082aec3218275eea60ee0f08
Niranjan <niranjan2992@gmail.com>
2023-08-04 01:06:31 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+  type        = string
+  description = "The subscription ID to be scanned"
+  default     = null
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "environment" {
+  default     = "dev"
+  description = "Must be all lowercase letters or numbers"
+}
+
+variable "min_tls_version" {
+  default     = "TLS1_2"
+  description = "Must be all lowercase letters or numbers"
+}
